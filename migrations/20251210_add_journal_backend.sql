22c4b12ad19f02d3dd8a860faacfe26ce6ff1f14
hisr2024 <hisr2024@gmail.com>
2025-12-04 10:00:18 +0100

    Fix journal entry id migration constraints

--- migrations/20251210_add_journal_backend.sql
+++ migrations/20251210_add_journal_backend.sql
@@ -10,12 +10,16 @@ ALTER TABLE IF EXISTS journal_entry_tags DROP CONSTRAINT IF EXISTS journal_entry
 ALTER TABLE IF EXISTS journal_versions DROP CONSTRAINT IF EXISTS journal_versions_entry_id_fkey;
 ALTER TABLE IF EXISTS journal_search_index DROP CONSTRAINT IF EXISTS journal_search_index_entry_id_fkey;
 ALTER TABLE IF EXISTS emotional_reset_sessions DROP CONSTRAINT IF EXISTS emotional_reset_sessions_journal_entry_id_fkey;
+ALTER TABLE IF EXISTS journal_entry_tags DROP CONSTRAINT IF EXISTS journal_entry_tags_pkey;
+ALTER TABLE IF EXISTS journal_entries DROP CONSTRAINT IF EXISTS journal_entries_pkey;
 
 ALTER TABLE IF EXISTS journal_entry_tags ALTER COLUMN IF EXISTS entry_id TYPE VARCHAR(64) USING entry_id::VARCHAR(64);
 ALTER TABLE IF EXISTS journal_versions ALTER COLUMN IF EXISTS entry_id TYPE VARCHAR(64) USING entry_id::VARCHAR(64);
 ALTER TABLE IF EXISTS journal_search_index ALTER COLUMN IF EXISTS entry_id TYPE VARCHAR(64) USING entry_id::VARCHAR(64);
 ALTER TABLE IF EXISTS emotional_reset_sessions ALTER COLUMN IF EXISTS journal_entry_id TYPE VARCHAR(64) USING journal_entry_id::VARCHAR(64);
 ALTER TABLE IF EXISTS journal_entries ALTER COLUMN IF EXISTS id TYPE VARCHAR(64) USING id::VARCHAR(64);
+ALTER TABLE IF EXISTS journal_entries ADD CONSTRAINT journal_entries_pkey PRIMARY KEY (id);
+ALTER TABLE IF EXISTS journal_entry_tags ADD CONSTRAINT journal_entry_tags_pkey PRIMARY KEY(entry_id, tag_id);
 
 ALTER TABLE IF EXISTS journal_entry_tags
     ADD CONSTRAINT journal_entry_tags_entry_id_fkey
